1a12b6ffbbe2b71dfdb2c2b653ad718eb84f74c1
Hans Opoku <88454205+HansOheneba@users.noreply.github.com>
2024-06-07 09:35:23 +0000

    Job listing added

---
+++ resources/views/jobs.blade.php
@@ -0,0 +1,13 @@
+<x-layout>
+    <x-slot:heading>
+        About Page
+    </x-slot:heading>
+
+    @foreach ($jobs as $job)
+        <li>
+            <a href="/jobs/">
+                <strong>{{ $job['title'] }}</strong> : Pays {{ $job['salary'] }} per year
+            </a>
+             </li>
+    @endforeach
+</x-layout>
--- routes/web.php
+++ routes/web.php
@@ -1,17 +1,56 @@
 <?php
 
+use Illuminate\Support\Arr;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('home', [
-        'greeting'=>'Yo',
-        'name'=>'Hans Oheneba'
+    return view('home');
+});
+Route::get('/jobs', function () {
+    return view('jobs', [
+        'jobs' => [
+            [
+                'id' => 1,
+                'title' => 'Director',
+                'salary' => '$50,000'
+            ],
+            [
+                'id' => 2,
+                'title' => 'Coder',
+                'salary' => '$40,000'
+            ],
+            [
+                'id' => 3,
+                'title' => 'Tutor',
+                'salary' => '$90,000'
+            ]
+        ]
     ]);
 });
-Route::get('/about', function () {
-    return view('about');
-});
 
+Route::get('/jobs/{id}', function ($id) {
+    $jobs = [
+        [
+            'id' => 1,
+            'title' => 'Director',
+            'salary' => '$50,000'
+        ],
+        [
+            'id' => 2,
+            'title' => 'Coder',
+            'salary' => '$40,000'
+        ],
+        [
+            'id' => 3,
+            'title' => 'Tutor',
+            'salary' => '$90,000'
+        ]
+    ];
+   $job = Arr::first($jobs, fn($job) => $job['id'] == $id);
+
+
+   return view('job', ['job'=> $job]);
+});
 Route::get('/contact', function () {
     return view('contact');
 });
